963be3852b2022675002d4e46fb7a0f65c9bd2eb
daniellyncas <daniel.l@lyncas.net>
2021-03-14 08:20:35 -0300

    feature: project structure to study redux

---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Wrapper } from './AppHeader.styles'
+
+function AppHeader () {
+  return <Wrapper>
+    Master
+    <span>
+      Shopping
+    </span>
+  </Wrapper>
+}
+
+export default AppHeader
